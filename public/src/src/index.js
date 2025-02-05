2f6fd8d60e7a499fb5c79cc89c7e6447684f79f0
Herdi88 <127148030+Herdi88@users.noreply.github.com>
2025-02-05 11:37:28 +0300

    Create index.js

---
+++ public/src/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import FMCDashboard from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <FMCDashboard />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
